0c6bab79a8f27849a2760ddbc962e02e64d4289d
nath-codes <nathantredgett@gmail.com>
2017-07-18 14:42:27 +0100

    created basic footer component with current year and lorem

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <div className="l-footer" />;
+export default class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      year: new Date().getFullYear(),
+    };
+  }
+
+  render() {
+    return (
+      <footer>
+        <div className="l-container">
+          Lorem ipsum dolor sit amet {this.state.year}
+        </div>
+      </footer>
+    );
+  }
+}
