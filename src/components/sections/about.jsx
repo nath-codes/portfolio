fe6cf81045f337618b09e1e717b572d8b1fb2d97
nath-codes <nathantredgett@gmail.com>
2017-07-18 14:58:17 +0100

    all img tags now scale their container by default

---
+++ src/components/sections/about.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <section className="l-section">
+        <div className="l-container">
+          <h2 className="heading">About</h2>
+          <div className="copy">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Labore odit quam enim consequatur deleniti animi sunt.
+              Voluptatibus eaque dignissimos ex tempora expedita, ab reprehenderit quas!
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
