711323671e18e53884a150bb731eb2eccf6e4158
nath-codes <nathantredgett@gmail.com>
2017-07-19 13:42:57 +0100

    added basic styling to about section

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <section className="l-section">
+      <section className="l-section l-section--about">
         <div className="l-container">
           <div className="l-section__inner">
-            <h2 className="heading">About</h2>
-            <div className="copy">
+            <h2 className="heading heading--alt">About</h2>
+            <div className="copy copy--alt">
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Labore odit quam enim consequatur deleniti animi sunt.
