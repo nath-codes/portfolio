0d3ac58a62df96441f9cbf5131f395d3f1e92ec7
nath-codes <nathantredgett@gmail.com>
2017-07-20 20:20:56 +0100

    adding basic backend component

--- src/components/layout/main.jsx
+++ src/components/layout/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../sections/about';
+import Backend from '../sections/backend';
 import Contact from '../sections/contact';
 
 export default () => (
   <main className="l-main">
     <About />
+    <Backend />
     <Contact />
   </main>
 );
---
+++ src/components/sections/backend.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Backend extends Component {
+  render() {
+    return (
+      <section className="l-section l-section--backend">
+        <div className="l-container">
+          <div className="l-section__inner">
+            <h2 className="heading heading--alt">Backend</h2>
+            <div className="copy copy--alt">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Labore odit quam enim consequatur deleniti animi sunt.
+                Voluptatibus eaque dignissimos ex tempora expedita, ab reprehenderit quas!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
