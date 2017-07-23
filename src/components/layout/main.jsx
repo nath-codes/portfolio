da0f7b0ded7d50f008a8831d72dca1b465ae1044
nath-codes <nathantredgett@gmail.com>
2017-07-23 15:15:39 +0100

    adding devops component with basic styling ... maybe a bit too garish though

--- src/components/layout/main.jsx
+++ src/components/layout/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import About from '../sections/about';
 import Backend from '../sections/backend';
 import Contact from '../sections/contact';
+import DevOps from '../sections/devops';
 
 export default () => (
   <main className="l-main">
     <About />
     <Backend />
+    <DevOps />
     <Contact />
   </main>
 );
---
+++ src/components/sections/devops.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class DevOps extends Component {
+  render() {
+    return (
+      <section className="l-section l-section--devops">
+        <div className="l-container">
+          <div className="l-section__inner">
+            <h2 className="heading heading--alt">Dev Ops</h2>
+            <div className="copy copy--alt">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Labore odit quam enim consequatur deleniti animi sunt.
+                Voluptatibus eaque dignissimos ex tempora expedita, ab reprehenderit quas!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
