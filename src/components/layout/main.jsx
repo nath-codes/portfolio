788c97993d9e9027bda3e693823ab3141c88dde5
Nathan Tredgett <nathan.tredgett@gdsinternational.com>
2017-08-02 10:53:53 +0100

    added basic frontend component

--- src/components/layout/main.jsx
+++ src/components/layout/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from '../sections/about';
+import Frontend from '../sections/frontend';
 import Backend from '../sections/backend';
 import Contact from '../sections/contact';
 import DevOps from '../sections/devops';
@@ -7,6 +8,7 @@ import DevOps from '../sections/devops';
 export default () => (
   <main className="l-main">
     <About />
+    <Frontend />
     <Backend />
     <DevOps />
     <Contact />
---
+++ src/components/sections/frontend.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Frontend extends Component {
+  render() {
+    return (
+      <section className="l-section">
+        <div className="l-container">
+          <div className="l-section__inner">
+            <h2 className="heading">Frontend</h2>
+            <div className="copy">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Labore odit quam enim consequatur deleniti animi sunt.
+                Voluptatibus eaque dignissimos ex tempora expedita, ab reprehenderit quas!
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    );
+  }
+}
