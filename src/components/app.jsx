6b44f3f53a3bb082a67b2df1cb12f64b44ac6d8b
Nathan Tredgett <nathan.tredgett@gdsinternational.com>
2017-08-02 10:46:28 +0100

    removing footer as the final 'contact' section does everything the footer does anyway

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Header from './layout/header';
 import Main from './layout/main';
-import Footer from './layout/footer';
 import '../styles/main.scss';
 
 export default () => (
@@ -16,6 +15,5 @@ export default () => (
     </Helmet>
     <Header />
     <Main />
-    <Footer />
   </div>
 );
