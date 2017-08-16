c2c3ebe01e7c78a612cdbdb5fd0cf3c85e6a5fc4
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-16 21:19:59 +0100

    added overlay to hero background video as it previously made the hero title difficult to read

--- src/components/video-background.jsx
+++ src/components/video-background.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 const Video = props => (
   <div>
+    <div className="overlay overlay--hero" />
     <video
       className="video-background"
       src={props.src}
