6281069c5576f09cfa75f99c166623195244194c
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-16 15:21:36 +0100

    refactoring carousel data to include 'id', which will be used for react list 'key', as well as a style class

--- src/data/frontend-carousel.js
+++ src/data/frontend-carousel.js
@@ -1,8 +1,20 @@
 const frontEndCarousel = [
-  'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/react.svg',
-  'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/gulp.svg',
-  'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/webpack.svg',
-  'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/node-sass.svg',
+  {
+    id: 'react',
+    src: 'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/react.svg',
+  },
+  {
+    id: 'gulp',
+    src: 'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/gulp.svg',
+  },
+  {
+    id: 'webpack',
+    src: 'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/webpack.svg',
+  },
+  {
+    id: 'sass',
+    src: 'https://s3-eu-west-1.amazonaws.com/cdn.nath.codes/src/img/node-sass.svg',
+  },
 ];
 
 export default frontEndCarousel;
