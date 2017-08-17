28b4f4e0a6bda2757b0f4c389f5b357e28879ed7
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-17 10:47:28 +0100

    added projects data object used to store project meta data. This will be removed in v2.0 when this data is returned via an API

---
+++ src/data/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'foo.com',
+    url: 'http://foo.com',
+    tags: ['developement'],
+  },
+];
+
+export default projects;
