f505776b4f9f21f4a83dcb398f31bfb69930eca5
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-18 11:21:11 +0100

    added mroe dummy project data for testing

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,37 @@ const projects = [
     img: 'http://via.placeholder.com/350x150',
     title: 'foo.com',
     url: 'http://foo.com',
-    tags: ['developement'],
+    tags: ['react'],
+  },
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'bar.com',
+    url: 'http://bar.com',
+    tags: ['wordpress'],
+  },
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'foo2.com',
+    url: 'http://foo2.com',
+    tags: ['react'],
+  },
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'bar2.com',
+    url: 'http://bar2.com',
+    tags: ['wordpress'],
+  },
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'foo3.com',
+    url: 'http://foo3.com',
+    tags: ['react'],
+  },
+  {
+    img: 'http://via.placeholder.com/350x150',
+    title: 'bar3.com',
+    url: 'http://bar3.com',
+    tags: ['wordpress'],
   },
 ];
 
