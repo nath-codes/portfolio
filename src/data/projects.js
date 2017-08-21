d3cd9117dede8647ac510c113d41e6d9ce6a9597
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-21 09:26:56 +0100

    resized desktop project image size to give them a bot more impact

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,36 @@
 const projects = [
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'foo.com',
     url: 'http://foo.com',
     tags: ['react'],
   },
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'bar.com',
     url: 'http://bar.com',
     tags: ['wordpress'],
   },
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'foo2.com',
     url: 'http://foo2.com',
     tags: ['react'],
   },
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'bar2.com',
     url: 'http://bar2.com',
     tags: ['wordpress'],
   },
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'foo3.com',
     url: 'http://foo3.com',
     tags: ['react'],
   },
   {
-    img: 'http://via.placeholder.com/350x150',
+    img: 'http://via.placeholder.com/630x730',
     title: 'bar3.com',
     url: 'http://bar3.com',
     tags: ['wordpress'],
