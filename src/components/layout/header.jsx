a0948edd63eabe4dbd7045e1be23b6d1cbdccbab
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-22 20:47:34 +0100

    added toggle state / animation for nav icon

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -4,13 +4,30 @@ import nav from '../../data/nav';
 import menuSvg from '../../menu.svg';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navToggled: false,
+    };
+
+    this.handleNavToggle = this.handleNavToggle.bind(this);
+  }
+
+  handleNavToggle() {
+    this.setState({ navToggled: !this.state.navToggled });
+  }
+
   render() {
+    const isToggled = this.state.navToggled ? 'is-active' : '';
+
     return (
       <header className="l-header">
         <div className="l-container l-container--nav flex flex--right">
           <Nav items={nav} itemClass="nav__item--l" />
           <div
-            className="nav__icon is-active"
+            className={`nav__icon ${isToggled}`}
+            onClick={this.handleNavToggle}
             dangerouslySetInnerHTML={{ __html: menuSvg }}
           />
         </div>
