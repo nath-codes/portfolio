d8e617fbd2afe91e82f1c51e3c899b09220c8eb4
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-22 20:31:06 +0100

    added the ever lovable hamburger menu to header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Nav from '../nav';
 import nav from '../../data/nav';
+import menuSvg from '../../menu.svg';
 
 export default () => (
   <header className="l-header">
     <div className="l-container">
       <Nav items={nav} />
+      <div
+        className="nav__icon"
+        dangerouslySetInnerHTML={{ __html: menuSvg }}
+      />
     </div>
   </header>
 );
