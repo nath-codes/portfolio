929ab8b98661b000ca34eb973702b4161cf3d9c9
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-22 20:38:03 +0100

    refactored header component from function to class component as it will need to manage state of toggled burger menu

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Nav from '../nav';
 import nav from '../../data/nav';
 import menuSvg from '../../menu.svg';
 
-export default () => (
-  <header className="l-header">
-    <div className="l-container l-container--nav flex flex--right">
-      <Nav items={nav} itemClass="nav__item--l" />
-      <div
-        className="nav__icon"
-        dangerouslySetInnerHTML={{ __html: menuSvg }}
-      />
-    </div>
-  </header>
-);
+class Header extends Component {
+  render() {
+    return (
+      <header className="l-header">
+        <div className="l-container l-container--nav flex flex--right">
+          <Nav items={nav} itemClass="nav__item--l" />
+          <div
+            className="nav__icon is-active"
+            dangerouslySetInnerHTML={{ __html: menuSvg }}
+          />
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
