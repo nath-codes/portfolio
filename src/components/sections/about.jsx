f03c3df8cd731c394b4e93dc0865f3ceb2421256
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-26 10:46:23 +0100

    added about copy

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -9,9 +9,14 @@ export default class About extends Component {
             <h2 className="heading heading--alt">About</h2>
             <div className="copy copy--alt">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Labore odit quam enim consequatur deleniti animi sunt.
-                Voluptatibus eaque dignissimos ex tempora expedita, ab reprehenderit quas!
+                I’ve been developing websites professionally for nearly ten years.
+                I love what I do and I have been lucky to work on a variety of projects,
+                big to small, cool to ... not so cool.
+              </p>
+              <p>
+                This website highlights some of my more recent work,
+                as well as giving you a brief overview of what languages
+                and frameworks I use when I’m developing.
               </p>
             </div>
           </article>
