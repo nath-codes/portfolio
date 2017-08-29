75bba00c24a42c68e9358115edcae1246da1c9e6
Nathan Tredgett <nathantredgett@gmail.com>
2017-08-29 17:00:08 +0100

    moved meta data to index.html and removed react-helmet package as the site is one page so does not need to be dynamic

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import Header from './layout/header';
 import Main from './layout/main';
 import '../styles/main.scss';
 
 export default () => (
   <div>
-    <Helmet>
-      <title>Nath.codes | Portfolio</title>
-      <meta
-        name="description"
-        content="The portfolio of Bristol web developer Nathan Tredgett."
-      />
-    </Helmet>
     <Header />
     <Main />
   </div>
