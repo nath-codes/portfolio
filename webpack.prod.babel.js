16ed257b2ed536c097cb0a70ec73a6eb7680ceb5
Nathan Tredgett <nathantredgett@gmail.com>
2017-09-02 09:05:07 +0100

    added uglifyjs plugin for minifying production builds

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -1,4 +1,9 @@
 import merge from 'webpack-merge';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 import common from './webpack.common.babel';
 
-export default merge(common, {});
+export default merge(common, {
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
+});
