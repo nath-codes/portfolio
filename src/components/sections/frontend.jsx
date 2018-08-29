5ccb8498bbe5d1bd5769851fa7af234abc45eaa8
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-29 10:51:32 +0100

    updated Frontend copy

--- src/components/sections/frontend.jsx
+++ src/components/sections/frontend.jsx
@@ -21,24 +21,33 @@ export default class Frontend extends Component {
     const background = this.state.backgroundClass;
 
     return (
-      <section id="frontend" className={`l-section l-section--frontend l-section--${background}`}>
+      <section
+        id="frontend"
+        className={`l-section l-section--frontend l-section--${background}`}
+      >
         <div className="l-container l-container--frontend flex flex--between">
           <article className="article">
             <h2 className="heading heading--alt">Frontend</h2>
             <div className="copy copy--alt copy--frontend">
               <p>
-                My current frontend framework of choice is React,
-                but I cut my teeth with vanilla javascript and frameworks,
-                such as Backbone and Marionette.
+                My current frameworks of choice are React and Vue, but I am more
+                then happy using vanilla JavaScript, as well as older
+                frameworks, such as Backbone and Marionette.
               </p>
+              <p>For testing I use either Jest and Enzyme or Mocha and Chai.</p>
               <p>
-                Styling is handled via SCSS, made scaleable with SCMACCS and BEM architecture.
-                My current build tool is Webpack, but before that it was Gulp and Grunt.
+                Styling is usually handled via Styled Components, but I have had
+                plenty of experience with SCSS, made scaleable with SCMACCS and
+                BEM architecture. My current build tool is Webpack, but before
+                that it was Gulp and Grunt.
               </p>
-
             </div>
           </article>
-          <Carousel slides={slides} delay="2500" onTransition={this.handleTransition} />
+          <Carousel
+            slides={slides}
+            delay="2500"
+            onTransition={this.handleTransition}
+          />
         </div>
       </section>
     );
